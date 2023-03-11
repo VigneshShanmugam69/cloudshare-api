c5b403181010ca851bda74f6f07e010b257f867d
mahalakshmimonisha123 <118173387+mahalakshmimonisha123@users.noreply.github.com>
2023-03-11 16:36:44 +0530

    api for bucket crud operations

--- NodeAPI/dist/server.js
+++ NodeAPI/dist/server.js
@@ -4,10 +4,12 @@ const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const Users_1 = require("./Users");
+const Users_2 = require("./Bucket");
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 app.use(Users_1.router);
+app.use(Users_2.router);
 app.listen(4201, () => {
     return console.log('My Node App listening on port 4201');
 });
