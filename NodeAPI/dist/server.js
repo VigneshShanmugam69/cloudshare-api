0b5d5a8abe5561bbe16c4e7cc4b3800e1f9d6c81
VigneshShanmugam69 <vigneshshanmugam69@gmail.com>
2023-03-15 16:31:26 +0530

    List users and Create user

--- NodeAPI/dist/server.js
+++ NodeAPI/dist/server.js
@@ -6,12 +6,14 @@ const bodyParser = require("body-parser");
 const user = require("./Users");
 const bucket = require("./Bucket");
 const objects = require("./Objects");
+const authorization=require("./authorization");
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 app.use(user.router);
 app.use(bucket.router);
 app.use(objects.router);
+app.use(authorization.router);
 app.listen(4201, () => {
     return console.log('CloudShare server started...');
 });
