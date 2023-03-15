3af726330c3790caf93424dfeafe0e03b0e09552
mahalakshmimonisha123 <118173387+mahalakshmimonisha123@users.noreply.github.com>
2023-03-15 13:03:50 +0530

    Task-312 Bucket tag

--- NodeAPI/dist/server.js
+++ NodeAPI/dist/server.js
@@ -3,14 +3,14 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
-const Users_1 = require("./Users");
-const Users_2 = require("./Bucket");
-const Objects = require("./Objects");
+const Users_1 = require("./users");
+const Buckets = require("./bucket");
+const Objects = require("./objects");
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 app.use(Users_1.router);
-app.use(Users_2.router);
+app.use(Buckets.router);
 app.use(Objects.router);
 app.listen(4201, () => {
     return console.log('CloudShare server started...');
