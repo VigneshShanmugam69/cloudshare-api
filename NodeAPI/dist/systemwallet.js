58470da2fa51bd80ff9c1aaf188e18c6734c648b
VigneshShanmugam69 <vigneshshanmugam69@gmail.com>
2023-04-04 17:51:09 +0530

    List user from OKTA group and sync users from OKTA to local database

--- NodeAPI/dist/systemwallet.js
+++ NodeAPI/dist/systemwallet.js
@@ -2,8 +2,10 @@ const express_1 = require("express");
 const AWS = require('aws-sdk');
 const connect = require('./dbconnection');
 const ejs = require('ejs');
+const generator = require('generate-password');
 const keytar = require('keytar');
 const okta = require('@okta/okta-sdk-nodejs');
+const mail=require('./email');
 exports.router = (0, express_1.Router)();
 
 exports.router.post('/storeTheCredential', async (req, res) => {
@@ -47,7 +49,7 @@ exports.router.post('/getUserGroups', async (req, res) => {
     try {
         const authClient = new okta.Client({
             orgUrl: 'https://dev-99932483.okta.com',
-            issuer: 'https://dev-33160744.okta.com/oauth2/default',
+            issuer: 'https://dev-99932483.okta.com/oauth2/default',
             token: '008DWbCPRmqViVAJXrcYmeHDEVUTEnatX66-FDQwvd',
             redirectUri: 'http://127.0.0.1:4201/callback'
         });
@@ -66,7 +68,7 @@ exports.router.post('/getAllGroups', async (req, res) => {
     try {
         const authClient = new okta.Client({
             orgUrl: 'https://dev-99932483.okta.com',
-            issuer: 'https://dev-33160744.okta.com/oauth2/default',
+            issuer: 'https://dev-99932483.okta.com/oauth2/default',
             token: '008DWbCPRmqViVAJXrcYmeHDEVUTEnatX66-FDQwvd',
             redirectUri: 'http://127.0.0.1:4201/callback'
         });
@@ -77,5 +79,74 @@ exports.router.post('/getAllGroups', async (req, res) => {
     }
     catch (error) {
         res.send(error);
+    }
+});
+
+// List all users from group
+exports.router.post('/listGroupUsers',async(req,res)=>{
+    try {
+        const authClient = new okta.Client({
+            orgUrl: 'https://dev-99932483.okta.com',
+            issuer: 'https://dev-99932483.okta.com/oauth2/default',
+            token: '008DWbCPRmqViVAJXrcYmeHDEVUTEnatX66-FDQwvd',
+            redirectUri: 'http://127.0.0.1:4201/callback'
+        });
+        const groupname = req.body.groupname;
+        var groupId;
+        await authClient.listGroups({ q: groupname }).each(group => { groupId = (group.id); });
+        const groupusers = authClient.listGroupUsers(groupId);
+        const users = [];
+        await groupusers.each(user => { users.push(user.profile); });
+        res.send(users);
+    }
+    catch (error) {
+        res.send(error);
+    }
+});
+
+//Sync all users from one group to local database
+exports.router.post('/syncUserToApplication',async(req,res)=>{
+    try {
+        const authClient = new okta.Client({
+            orgUrl: 'https://dev-99932483.okta.com',
+            issuer: 'https://dev-99932483.okta.com/oauth2/default',
+            token: '008DWbCPRmqViVAJXrcYmeHDEVUTEnatX66-FDQwvd',
+            redirectUri: 'http://127.0.0.1:4201/callback'
+        });
+        const groupname = req.body.groupname;
+        var groupId;
+        await authClient.listGroups({ q: groupname }).each(group => { groupId = (group.id); });
+        const groupusers = authClient.listGroupUsers(groupId);
+        const users = [];
+        await groupusers.each(user => { users.push(user.profile); });
+
+        // Sync users from OKTA to local database
+        for (const user of users) {
+            var firstName = user.firstName;
+            var lastName = user.lastName;
+            var userName = user.login;
+            var email = user.email;
+            var roleID = 1; //user or admin
+            var password = generator.generate({ Number: true, length: 10 });
+
+            // connect the database and insert the new user details in user table
+            const connection = await (connect.connect)();
+            var sql = "INSERT INTO users (Firstname,Lastname,Email,Username,RoleID,Password,IsFirst) VALUES ?";
+            var values = [[firstName, lastName, email, userName, roleID, password, true]];
+            await connection.query(sql, [values]);
+            await mail.mail(email, password, firstName, userName);
+        }
+        let obj = {
+            "status": 1,
+            "message": "User synced successfully"
+        }
+        res.send(obj);
+    }
+    catch (err) {
+        let obj = {
+            "status": 2,
+            "message": "Failed to sync user"
+        }
+        res.send(obj);
     }
 });
