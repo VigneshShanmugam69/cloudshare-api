b38e8046aa86ac878d6417daad8fa3d6ff72e2d4
VigneshShanmugam69 <vigneshshanmugam69@gmail.com>
2023-05-02 17:51:39 +0530

    list account from aws organization

--- NodeAPI/dist/sso.js
+++ NodeAPI/dist/sso.js
@@ -1,5 +1,8 @@
 const authClient = require('@okta/okta-auth-js');
 const express = require("express");
+const axios = require('axios');
+const AWS = require('aws-sdk');
+const sts = new AWS.STS();
 exports.router = (express.Router)();
 
 exports.router.post('/authenticateUser', async (req, res) => {
@@ -39,3 +42,145 @@ async function authenticateUser(Username, Password) {
         return obj;
     }
 }
+
+
+exports.router.post('/getCredentials', async (req, res) => {
+  try {
+    const org=new AWS.Organizations({region:'us-east-1'})
+   await org.listAccounts({}, function(err, data) {
+        if (err) console.log(err, err.stack);
+        else {
+          console.log('List of accounts:');
+          console.log(data);
+          data.Accounts.forEach(account => {
+            console.log(`${account.Name} (${account.Id})`);
+          });
+        }
+      });
+
+      const sts = new AWS.STS();
+
+const accountId = '977258277033'; // Replace with the AWS account ID you want to assume a role in
+const roleName = 'myRole'; 
+const params = {
+  RoleArn: `arn:aws:iam::${accountId}:role/${roleName}`,
+  RoleSessionName: 'mySession'
+};
+
+await sts.assumeRole(params, function(err, data) {
+  if (err) {
+    console.log('Error assuming role', err);
+  } else {
+    const accessKeyId = data.Credentials.AccessKeyId;
+    const secretAccessKey = data.Credentials.SecretAccessKey;
+    const sessionToken = data.Credentials.SessionToken;
+    console.log(`Access key ID: ${accessKeyId}`);
+    console.log(`Secret access key: ${secretAccessKey}`);
+    console.log(`Session token: ${sessionToken}`);
+  }
+});
+res.send('success');
+
+//   const sso=new AWS.SSO();
+//   sso.listAccounts((err,data)=>{
+// if(err){
+//     res.send(err);
+// }
+// res.send(data);
+//   });
+
+        // res.send(accesstoken);
+  } catch (error) {
+    res.send(error);
+  }
+});
+
+
+  //   const oktaOrgUrl = 'https://dev-99932483.okta.com';
+//       const oktaApiClientToken = '008DWbCPRmqViVAJXrcYmeHDEVUTEnatX66-FDQwvd';
+//       const oktaUsername = 'vigneshshanmugam9@gmail.com';
+//       const oktaPassword = 'Vignesh@9';
+//       const ssoInstanceArn = 'arn:aws:sso:::instance/ssoins-7223f68b1e33a57f';
+//       const storeId = 'd-90677f5a16';
+
+//   const oktaClient = new authClient.OktaAuth({
+//           orgUrl: oktaOrgUrl,
+//           issuer: 'https://dev-99932483.okta.com/oauth2/default',
+//           clientId:'0oa96jm6l9Xbt6lmy5d7',
+//           token: oktaApiClientToken,
+//           // redirectUri:'http:/localhost:4201/',
+//           scopes: ['openid', 'profile', 'email'],
+//           audience: 'api://default',
+//         });
+//       var options={
+//         orgUrl: oktaOrgUrl,
+//         issuer: 'https://dev-99932483.okta.com/oauth2/default',
+//         clientId:'0oa96jm6l9Xbt6lmy5d7',
+//         token: oktaApiClientToken,
+//         scopes: ['openid', 'profile', 'email'],
+//         audience: 'api://default',
+//         pkce:true,
+//     grant_type:'authorization_code'
+//       };
+
+//         const accesstoken= oktaClient.getAccessToken(options);
+// const token=await authClient.getToken(options)
+
+
+
+      // await sts.assumeRole({
+      //   RoleArn: `arn:aws:iam::${ssoInstanceArn}:role/aws-reserved/sso.amazonaws.com/${storeId}`,
+      //   RoleSessionName: 'my-session',
+      //   DurationSeconds: 3600 // Set the duration of the temporary credentials
+      // }, (err, data) => {
+      //   if (err) {
+      //     console.error(err);
+      //     return;
+      //   }
+      //   console.log(data)
+      //   const s3 = new AWS.S3({
+      //       accessKeyId: data.Credentials.AccessKeyId,
+      //       secretAccessKey: data.Credentials.SecretAccessKey,
+      //       sessionToken: data.Credentials.SessionToken
+      //     });});
+
+      // const oktaClient = new authClient.OktaAuth({
+      //     orgUrl: oktaOrgUrl,
+      //     issuer: 'https://dev-99932483.okta.com/oauth2/default',
+      //     clientId:'0oa91fv3ncEaofktD5d7',
+      //     token: oktaApiClientToken
+      //   });
+        
+      //   const oktaCredentials = {
+      //     username: oktaUsername,
+      //     password: oktaPassword
+      //   };
+
+  
+      //   const response=await oktaClient.signInWithCredentials(oktaCredentials)
+
+      //   const config = {
+      //       headers: {
+      //         'Authorization': 'SSWS ' + response.sessionToken
+      //       }
+      //     };
+
+     
+
+      //   const accessToken=await oktaClient.token.getUserInfo(response.sessionToken)
+
+
+
+
+
+
+// const response1=await  axios.post('https://dev-99932483.okta.com/api/v1/sessions/me', {}, config)
+// .then(response => {
+//   console.log(response);
+//   const accessToken = response.data.access_token.token;
+//   // Use the access token as needed
+// })
+// .catch(error => {
+//   console.error(error);
+// });
+// console.log(response1)
