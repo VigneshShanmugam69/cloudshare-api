18f98befd04aa17f141f5aa4f53416f05f0c84a8
mahalakshmimonisha123 <118173387+mahalakshmimonisha123@users.noreply.github.com>
2023-05-04 16:08:19 +0530

    Task:1205 Development: Modification in Copy and Move object API's

--- NodeAPI/dist/objectmenu.js
+++ NodeAPI/dist/objectmenu.js
@@ -1,7 +1,6 @@
 const express = require("express");
 exports.router = (0, express.Router)();
 const AWS = require('aws-sdk');
-var async = require('async');
 
 const s3 = new AWS.S3({
     accessKeyId: 'AKIA6HCJB5CUWQJCY3FQ',
@@ -18,80 +17,18 @@ exports.router.post('/copyobject', async (req, res) => {
     // source folder or source object
     const destinationBucket = payload.destinationBucket;
     const destinationKeyName = payload.destinationKeyName;
+    const listObject = await list(sourceBucket, sourceKeyName, destinationBucket, destinationKeyName);
 
-    const listObjectParams = {
-        Bucket: sourceBucket,
-        Prefix: sourceKeyName
-    }
     try {
-        s3.listObjectsV2(listObjectParams, async (err, data) => {
-            if (err) {
-                var error = {
-                    Error: err.code
-                };
-                res.send({ Result: error });
-            }
-            else {
-
-                (data.Contents.forEach(async (object) => {
-                    const copyObjectParams = {
-                        Bucket: destinationBucket,
-                        CopySource: `${sourceBucket}/${object.Key}`,
-                        Key: `${destinationKeyName}${object.Key.replace(sourceKeyName, '')}`
-                    };
-                    s3.copyObject(copyObjectParams, (err, data) => {
-                        if (err) {
-
-                            var error = {
-                                Error: err.code
-                            };
-                            res.send({ Result: error });
-
-                        } else {
-                            var obj = {
-                                CopySourceVersionId: data.CopySourceVersionId,
-                                VersionId: data.VersionId,
-                                ServerSideEncryption: data.ServerSideEncryption,
-                                ETag: data.CopyObjectResult.ETag,
-                                LastModified: data.CopyObjectResult.LastModified.toUTCString()
-                            };
-
-                            res.send({ Result: obj });
-                        }
-                    })
-                }));
-            }
-        })
+        res.send(listObject);
     } catch (err) {
         var error = {
             Error: err.code
         }
         res.send({ Result: error });
-
     }
-
 })
-
-// Move folder and the object from source bucket to destination bucket
-
-
-
-exports.router.post('/moveObject', async (req, res) => {
-    try {
-        const payload = req.body
-        const copyObject = await copy(payload.sourceBucket, payload.sourceKeyName, payload.destinationBucket,payload.destinationKeyName)
-        const deleteObject = await deleteObjects(payload.sourceBucket,payload.sourceKeyName)
-        res.send({ Result: copyObject });
-
-    } catch (err) {
-        // let obj = {
-        //     Error: err.Code
-        // }
-        res.send({ Result: err });
-    }
-
-})
-function copy(sourceBucket, sourceKeyName, destinationBucket,destinationKeyName) {
+function list(sourceBucket, sourceKeyName, destinationBucket, destinationKeyName) {
     return new Promise(async (resolve, reject) => {
         const listObjectParams = {
             Bucket: sourceBucket,
@@ -102,25 +39,46 @@ function copy(sourceBucket, sourceKeyName, destinationBucket,destinationKeyName)
                 var error = {
                     Error: err.code
                 };
-                resolve(error);
-
+                resolve({ Result1: error });
             }
             else {
-
                 (data.Contents.forEach(async (object) => {
+                    function sourcePath() {
+                        return new Promise(async (resolve, reject) => {
+                            const params = {
+                                Bucket: destinationBucket,
+                                Prefix: destinationKeyName
+                            }
+
+                            s3.listObjectsV2(params, function (err, value) {
+                                if (err) {
+                                    resolve.err
+                                } else {
+                                    if (params.Prefix) {
+                                        resolve(`${destinationKeyName}/${object.Key}${object.Key.replace(sourceKeyName, '')}`);
+                                    }
+                                    else {
+                                        resolve(`${sourceKeyName}${object.Key.replace(sourceKeyName, '')}`)
+                                    }
+
+                                }
+                            })
+                        })
+                    }
+                    const path = await sourcePath()
+
                     const copyObjectParams = {
                         Bucket: destinationBucket,
                         CopySource: `${sourceBucket}/${object.Key}`,
-                        Key: `${destinationKeyName}${object.Key.replace(sourceKeyName, '')}`
-                        // 1st folderName=destinationFolder, 2nd foldrName = source folder
+                        Key: `${path}`
+
                     };
                     s3.copyObject(copyObjectParams, (err, data) => {
                         if (err) {
-
                             var error = {
                                 Error: err.code
                             };
-                            resolve(error);
+                            resolve({ Result: error });
 
                         } else {
                             var obj = {
@@ -131,15 +89,105 @@ function copy(sourceBucket, sourceKeyName, destinationBucket,destinationKeyName)
                                 LastModified: data.CopyObjectResult.LastModified.toUTCString()
                             };
 
-
-                            resolve(obj);
+                            resolve({ Result: obj });
                         }
-                    });
-                }));
+                    })
+                }))
             }
-
-
         })
+    })
+}
+
+
+// Move folder and the object from source bucket to destination bucket
+
+exports.router.post('/moveObject', async (req, res) => {
+    try {
+        const payload = req.body
+        const copyObject = await copy(payload.sourceBucket, payload.sourceKeyName, payload.destinationBucket, payload.destinationKeyName)
+        const deleteObject = await deleteObjects(payload.sourceBucket, payload.sourceKeyName)
+        res.send({ Result: copyObject });
+
+    } catch (err) {
+        let obj = {
+            Error: err.Code
+        }
+        res.send({ Result: obj })
+    }
+
+})
+function copy(sourceBucket, sourceKeyName, destinationBucket, destinationKeyName) {
+    return new Promise(async (resolve, reject) => {
+        const listObjectParams = {
+            Bucket: sourceBucket,
+            Prefix: sourceKeyName
+        }
+        function list() {
+            return new Promise(async (resolve, reject) => {
+                s3.listObjectsV2(listObjectParams, async (err, data) => {
+                    if (err) {
+                        var error = {
+                            Error: err.code
+                        }
+                        resolve(error);
+                    }
+                    else {
+                        (data.Contents.forEach(async (object) => {
+                            function sourcePath() {
+                                return new Promise(async (resolve, reject) => {
+
+                                    const params = {
+                                        Bucket: destinationBucket,
+                                        Prefix: destinationKeyName
+                                    }
+
+                                    s3.listObjectsV2(params, function (err, value) {
+                                        if (err) {
+                                            resolve.err
+                                        } else {
+                                            if (params.Prefix) {
+                                                resolve(`${destinationKeyName}/${object.Key}${object.Key.replace(sourceKeyName, '')}`);
+                                            }
+                                            else {
+                                                resolve(`${sourceKeyName}${object.Key.replace(sourceKeyName, '')}`)
+                                            }
+
+                                        }
+                                    })
+                                })
+                            }
+                            const path = await sourcePath()
+                            const copyObjectParams = {
+                                Bucket: destinationBucket,
+                                CopySource: `${sourceBucket}/${object.Key}`,
+                                Key: `${path}`
+                            };
+                            s3.copyObject(copyObjectParams, (err, data) => {
+                                if (err) {
+
+                                    var error = {
+                                        Error: err.code
+                                    };
+                                    resolve(error);
+
+                                } else {
+                                    var obj = {
+                                        CopySourceVersionId: data.CopySourceVersionId,
+                                        VersionId: data.VersionId,
+                                        ServerSideEncryption: data.ServerSideEncryption,
+                                        ETag: data.CopyObjectResult.ETag,
+                                        LastModified: data.CopyObjectResult.LastModified.toUTCString()
+                                    };
+                                    resolve(obj);
+                                }
+                            })
+                        }))
+                    }
+                })
+            })
+        }
+        const listObject = await list();
+        resolve(listObject);
     })
 }
 
@@ -157,7 +205,6 @@ function deleteObjects(sourceBucket, sourceKeyName) {
                 resolve(error);
             }
             else {
-
                 (data.Contents.forEach(async (object) => {
                     const deleteObjectParams = {
                         Bucket: sourceBucket,
@@ -165,21 +212,16 @@ function deleteObjects(sourceBucket, sourceKeyName) {
                     };
                     s3.deleteObject(deleteObjectParams, (err, data) => {
                         if (err) {
-
                             var error = {
                                 Error: err.code
-                            };
+                            }
                             resolve(error);
-
                         } else {
-
                             resolve("Object deleted");
                         }
-                    });
-                }));
+                    })
+                }))
             }
-
-
         })
     })
 }
